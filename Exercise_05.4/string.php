58dbd963c921331a9d53aec564aea7aaf9379317
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-03-21 20:57:56 +0200

    Counting characters

---
+++ Exercise_05.4/string.php
@@ -0,0 +1,16 @@
+<?php
+$string1 = trim($_GET["string1"]);
+$string2 = trim($_GET["string2"]);
+
+$total = strlen($string1) + strlen($string2);
+
+$string1 = strip_tags("$string1");
+$string2 = strip_tags("$string2");
+
+if ($string1 == "" || $string2 == "") {
+    echo "You didn't give both strings!";
+} else {
+    echo "The passed strings were \"$string1\" and \"$string2\". Their combined length is $total characters!";
+}
+
+?>
