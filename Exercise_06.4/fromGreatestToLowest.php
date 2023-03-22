b197502d0ee135469f625103d2392aba1c7749e2
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-03-22 17:30:00 +0200

    From greatest to lowest

---
+++ Exercise_06.4/fromGreatestToLowest.php
@@ -0,0 +1,28 @@
+<?php
+$numbers = $_GET['numbers'];
+$arr = explode(',', $numbers);
+//$arr = array[8,65,5,32];
+//index       [0][1][2][3].....
+//count       (1)(2)(3)(4)....         
+
+echo "Order in the beginning: $numbers\n";
+
+// Your code here
+
+for ($i = 0; $i < count($arr) - 1; $i++) {
+    for ($j = $i + 1; $j < count($arr); $j++) {
+        if ($arr[$i] < $arr[$j]) {
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$j];
+            $arr[$j] = $temp;
+        }
+    }
+}
+$arr_rev = array_reverse($arr);
+
+$greatest_lowest = implode(',', $arr);
+$lowest_greatest = implode(',', $arr_rev);
+
+echo "From greatest to lowest: $greatest_lowest\n";
+echo "From lowest to greatest: $lowest_greatest\n";
+?>
