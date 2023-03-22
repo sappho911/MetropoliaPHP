86a9c9edfa8b452ea845faf986554cbfeec4de0d
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-03-22 15:51:37 +0200

    Address info

---
+++ Exercise_06.2/adressInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+$table = array(
+    "Eric Example" => "Example Lane 2",
+    "Cara Coder" => "Programming Path 5",
+    "Pete Programmer" => "Coding Street 5"
+);
+
+// Your code here
+
+$table["Eric Example"] = "Example Lane 10";
+$table["Cara Coder"] = "Coding Street 5";
+$table["Pete Programmer"] = "Programming Path 5";
+$table["Carl Coder"] = "Coding Street 5";
+
+
+print_r($table);
+?>
