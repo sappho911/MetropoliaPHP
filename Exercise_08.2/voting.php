a05b45ebede03cd79948f04aefeedbb56f641b20
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-03-28 18:06:36 +0300

    Voting

---
+++ Exercise_08.2/voting.php
@@ -0,0 +1,31 @@
+<?php
+$results = "results.txt";
+if (!file_exists($results))
+    die("Tiedostoa ei löydy!");
+
+$results_num = fopen($results, "r");
+
+$nominee = $_GET["nominee"];
+
+$temp = "";
+$voices = 0;
+
+while ($line = fgets($results_num, 1024)) {
+    $rows = explode("|", $line);
+    $voices = $voices + $rows[1];
+    if ($rows[0] != $nominee) {
+        $temp = $temp . $line;
+    } else {
+        $temp = $temp . $rows[0] . "|" . ($rows[1] + 1) . "\n";
+    }
+}
+
+$voices++;
+
+fclose($results_num);
+$results_writing = fopen($results, "w");
+fwrite($results_writing, $temp);
+fclose($results_writing);
+
+echo "Total votes: $voices ."
+    ?>
